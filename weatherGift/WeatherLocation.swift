ef6524ed550b85e7f70dfea29977dd4948ce1da4
Marissa <marissa.spletter@yahoo.com>
2021-10-10 13:57:54 -0400

    with git.ignore

---
+++ weatherGift/WeatherLocation.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherLocation.swift
+//  weatherGift
+//
+//  Created by Marissa Spletter on 10/8/21.
+//
+
+import Foundation
+
+class WeatherLocation: Codable {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    
+    init(name: String, latitude: Double, longitude: Double) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+    
+
+}
