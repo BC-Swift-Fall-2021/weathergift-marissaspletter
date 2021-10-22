f454ba25b222cce37ac08a190342dabf11fcaa38
Marissa <marissa.spletter@yahoo.com>
2021-10-22 13:10:02 -0400

    10.22.21

---
+++ weatherGift/DailyTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DailyTableViewCell.swift
+//  weatherGift
+//
+//  Created by Marissa Spletter on 10/22/21.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    @IBOutlet weak var dailySummaryView: UITextView!
+    
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryView.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+        }
+    }
+    
+}
--- weatherGift/LocationDetailViewController.swift
+++ weatherGift/LocationDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 private let dateFormatter: DateFormatter = {
     print("I just created a date formatter!")
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "EEEE, MMM, d, h:mm aaa"
+    dateFormatter.dateFormat = "EEEE, MMM, d"
     return dateFormatter
 } ()
 
@@ -22,6 +22,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var tableView: UITableView!
     
     var weatherDetail: WeatherDetail!
     //var weatherLocation: WeatherLocation!
@@ -30,9 +31,20 @@ class LocationDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        clearUserInterface()
+        
+        tableView.delegate = self
+        tableView.delegate = self
+        
         updateUserInterface()
     }
     
+    func clearUserInterface() {
+        dateLabel.text = ""
+        placeLabel.text = ""
+        temperatureLabel.text = ""
+        summaryLabel.text = ""
+        imageView.image = UIImage()
     
     func updateUserInterface() {
         
@@ -54,6 +66,7 @@ class LocationDetailViewController: UIViewController {
                 self.temperatureLabel.text = "\(self.weatherDetail.temperature)"
                 self.summaryLabel.text = self.weatherDetail.summary
                 self.imageView.image = UIImage(named: self.weatherDetail.dayIcon)
+                self.tableView.reloadData()
             }
         }
         
@@ -85,3 +98,24 @@ class LocationDetailViewController: UIViewController {
         pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true, completion: nil)
     }
 }
+
+extension LocationDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherDetail.dailyWeatherData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! DailyTableViewCell
+        //cell.dailyWeather = weatherDetail.dailyWeatherData[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+//extension LocationDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return weatherDetail.hourlyWeatherData.count
+//    }
